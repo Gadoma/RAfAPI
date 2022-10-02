bce3649a0116c66e0a7e1f7645cb464c189ccb6f
Gadoma <dev@gadoma.com>
2022-10-02 20:44:02 +0200

    021: tidy up docker setup

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,13 @@
 
 BASE_DIR=$(dirname "$(readlink -f "$0")")
 CONTAINER_NAME=rafapi
+DATA_SOURCE=db/db.dist.sqlite
 
 docker rm -f $CONTAINER_NAME &> /dev/null && echo ">>> previous instance of $CONTAINER_NAME was stopped and removed"
-docker run -d -p 5000:5000 --name $CONTAINER_NAME --restart unless-stopped $CONTAINER_NAME
+docker run \
+-d \
+--name $CONTAINER_NAME \
+-p 5000:5000 \
+--restart unless-stopped \
+-v $BASE_DIR/../$DATA_SOURCE:/app/$DATA_SOURCE \
+$CONTAINER_NAME
